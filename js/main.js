cb90cd35cc0613faaf048eb9a718dc9dc8f77c8d
AR1N <739190475@qq.com>
2018-10-13 21:18:29 +0800

    test

--- js/main.js
+++ js/main.js
@@ -68,32 +68,7 @@ window.onload = function () {
 //--------------------------------------------------------------------------
 
 $(function () {
-var mybody = document.getElementsByTagName('body')[0];
-var startX, startY, moveEndX, moveEndY, X, Y;
-mybody.addEventListener('touchstart', function (e) {
-    // e.preventDefault();
-    startX = e.touches[0].pageX;
-    startY = e.touches[0].pageY;
-}, false);
-mybody.addEventListener('touchmove', function (e) {
-    // e.preventDefault();
-    moveEndX = e.changedTouches[0].pageX;
-    moveEndY = e.changedTouches[0].pageY;
-    X = moveEndX - startX;
-    Y = moveEndY - startY;
-    var currentSection = $(".fp-section.active");
 
- if (Math.abs(Y) > Math.abs(X) && Y < 0) { // up
-    $("body").next(".fp-section .active").addClass("active").siblings().removeClass("active");
-    
- }
-    if (Math.abs(Y) > Math.abs(X) && Y > 0) { // down
-      
-    }
-   
-
-});
-  
     setTimeout(function () {
         $("audio").attr("src", "music/The most beautiful expectation.mp3")
     }, 4000); 
@@ -134,7 +109,8 @@ mybody.addEventListener('touchmove', function (e) {
         navigationTooltips: ["首页", "关于我", "技能", "联系我"], 
         navigationColor: '#fff', 
         slidesNavigation: true,
+       
     });
-   
+   $.fn.fullpage.setAutoScrolling(true);
     
 });
