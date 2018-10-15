a7e6e39aa44c9c29d4e83108c6b01893599d9b48
AR1N <739190475@qq.com>
2018-10-16 00:54:36 +0800

    scroll

--- js/main.js
+++ js/main.js
@@ -19,9 +19,7 @@ window.onload = function () {
             me.style.animation = "none";
         }
     }
-    //移动端
-
-
+   
 
     //背景切换
     var bgs = ["bg1_1.jpg", "bg1_2.jpg", "bg1_3.jpg", "bg1_4.jpg", "bg1_5.jpg"];
@@ -42,20 +40,20 @@ window.onload = function () {
         }
     }, 188);
 
-
-    //文字效果2     
-    var introduce = document.getElementById('introduce');
-    var introduceS = document.getElementById('introduceS');
-    r = 0;
-    timer2 = setInterval(function () {
-        introduceS.innerHTML = introduce.innerHTML.substring(0, r);
-        r++;
-        if (introduceS.innerHTML == introduce.innerHTML) {
-            clearInterval(timer2);
-        }
-    }, 150);
-
 }
+    //文字效果2    
+    // var introduce = document.getElementById('introduce');
+    // var introduceS = document.getElementById('introduceS');
+    // r = 0;
+    // timer2 = setInterval(function () {
+    //     introduceS.innerHTML = introduce.innerHTML.substring(0, r);
+    //     r++;
+    //     if (introduceS.innerHTML == introduce.innerHTML) {
+    //         clearInterval(timer2);
+    //     }
+    // }, 150);
+    
+
 //*********文档加载完成**************** */
 // document.onreadystatechange = completeLoading;
 // //加载状态为complete时移除loading效果
@@ -71,14 +69,28 @@ $(function () {
 
     setTimeout(function () {
         $("audio").attr("src", "music/The most beautiful expectation.mp3")
-    }, 4000);
+    }, 2000);
 
     //菜单按钮
     $(".smbtn i").click(function () {
         $(".menu_l").toggle();
         $(".menu ul").toggle();
     });
-
+//p2 pshow
+setTimeout(function(){
+    var r = 0;
+    var timer2 = setInterval(function () {
+        var t1 = $("#introduce").text();
+        var t2 = $("#introduceS").text();
+        var ts = $("#introduce").text().substr(0, r);
+        r++;
+        $("#introduceS").text(ts);
+        if (t1 == t2) {
+            clearInterval(timer2);
+        }
+    }, 88)
+},5000);
+   
     //联系方式 
     $(".contact i").hover(function () {
         $(this).siblings("span").stop().fadeIn();
@@ -109,12 +121,13 @@ $(function () {
         navigationTooltips: ["首页", "关于我", "技能", "联系我"],
         navigationColor: '#fff',
         slidesNavigation: true,
-
-    });
+     
+     });
+    //righttip
     $(".right a").hover(function () {
         $(this).siblings().fadeIn();
     }, function () {
         $(this).siblings().fadeOut();
     });
-
+       
 });
